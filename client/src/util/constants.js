b8e9c8f5aaec9d4a851d6531e888274a58d3ed98
alexpxrad <alexpxrad@gmail.com>
2022-10-06 17:18:05 -0400

    finishing the transaction setup

---
+++ client/src/util/constants.js
@@ -0,0 +1,7 @@
+import abi from "./Transactions.json"
+
+
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0xD9B50248fdcFd297770e89cf318D71E2B63d1c01' ;
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/fW7FdCkjXCiaQb28w-Jf3Dt2bPGdk2i6
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/fW7FdCkjXCiaQb28w-Jf3Dt2bPGdk2i6',
+      accounts: [ '39b5202db864e0a88f17e615fedd7f04931b75d10dbee578667538d2b31bb748'  ]
+    }
+  }
+}
