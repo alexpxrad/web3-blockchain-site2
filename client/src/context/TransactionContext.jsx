d99e0271ceaf8aaed91bb849befc9ac21ea8e7d8
alexpxrad <alexpxrad@gmail.com>
2022-10-08 17:48:43 -0400

    finishing transaction context functionality

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -12,15 +12,16 @@ const getEthereumContract = () => {
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(contractAddress, contractABI, signer)
 
-    console.log({
-        provider,
-        signer,  
-        transactionContract
-    })
+    return transactionContract
 }
 
 export const TransactionProvider = ({ children }) => {
     const [ currentAccount, setCurrentAccount ]= useState("")
+    const [ formData, setFormData ] = useState({ addressTo: '', amount: '', keyword: '', message: '' })
+
+    const handleChange = (e, name) => {
+        setFormData((prevState) => ({ ...prevState, [name]: e.target.value }))
+    }
 
     const checkIfWalletIsConnected = async () => {
         try {
@@ -62,12 +63,26 @@ export const TransactionProvider = ({ children }) => {
 
     }
 
+    const sendTransaction = async () => {
+        try {
+            if(!ethereum) return alert ("Please install metamask");
+
+            const { addressTo, amount, keyword, message } = formData; //get data from the form
+            const transactionContract = getEthereumContract();
+        } catch (error) {
+            console.log(error);
+
+            throw new Error("No ethereum object.")
+            
+        }
+    }
+
     useEffect(() => {
         checkIfWalletIsConnected();
     }, []);
 
     return (
-        <TransactionContext.Provider value={{ connectWallet, currentAccount }}>
+        <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction }}>
             {children}
         </TransactionContext.Provider>
     )
